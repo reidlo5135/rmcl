3677d80039414b510ce3d254873c957ffe8d04f8
reidlo5135 <201840103@email.daelim.ac.kr>
2023-12-14 16:58:56 +0900

    feat : add creating ros publisher by mqtt logics

--- rmcl/launch/rmcl_server.launch.py
+++ rmcl/launch/rmcl_server.launch.py
@@ -9,22 +9,14 @@ def generate_launch_description():
 
     package_name: str = 'rmcl'
     executable_name: str = 'rmcl_server'
+    parameter: str = os.path.join(get_package_share_directory(package_name=package_name), 'config', 'mqtt_connection_info.yaml')
 
     rmde_executor_node = Node(
         package=package_name,
         executable=executable_name,
         name=executable_name,
         output='screen',
-        parameters=[
-            {
-                'mqtt_broker_address': 'localhost',
-                'mqtt_broker_port': 1883,
-                'mqtt_client_name': '',
-                'mqtt_client_keep_alive': 60,
-                'mqtt_user_name': 'wavem',
-                'mqtt_user_password': '1234'
-            }
-        ]
+        parameters=[parameter]
     )
 
     ld.add_action(rmde_executor_node)
--- rmcl/setup.py
+++ rmcl/setup.py
@@ -19,6 +19,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
